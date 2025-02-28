9682c234214416aa1ef4defdffa58ffac895dbce
MUHAMMED FAYAZ T S <muhammedfayazts01@gmail.com>
2025-02-28 12:29:53 +0530

    :tada: initial setup client

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <div className='w-full h-screen bg-neutral-950 text-white flex justify-center items-center'>
+      <h1 className=''>TODO: setup client
+        <Button variant={"default"} > test</Button>
+      </h1>
+    </div>
+  )
+}
+export default App
